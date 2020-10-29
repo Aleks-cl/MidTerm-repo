628e69c411fd00a42a9331bee630eb35c72ae11b
Aleks-cl <71607579+Aleks-cl@users.noreply.github.com>
2020-10-29 19:38:13 -0400

    first commit2

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://301075992Aleks:zDwbCswFy1GcyrDK@midtermcluster.hm8xo.mongodb.net/books229?retryWrites=true&w=majority"
+};
